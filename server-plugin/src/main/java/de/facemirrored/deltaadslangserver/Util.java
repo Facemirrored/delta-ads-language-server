7fddf482a8bc69cb268a49ad89bc9263ca350c03
facemirrored <herrmann.bjarne@outlook.de>
2022-12-25 20:21:31 +0100

    Refactor to multi-module maven project

---
+++ server-plugin/src/main/java/de/facemirrored/deltaadslangserver/Util.java
@@ -0,0 +1,12 @@
+package de.facemirrored.deltaadslangserver;
+
+public class Util {
+
+    private Util() {
+        // static access
+    }
+
+    public static boolean not(final boolean arg) {
+        return !arg;
+    }
+}
